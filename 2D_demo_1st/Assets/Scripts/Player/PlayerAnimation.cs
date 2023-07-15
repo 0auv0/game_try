1d86cd49a85323ec5f9aff4a9f08313afd97c16c
auv <591561890@qq.com>
2023-07-15 18:33:27 +0800

    Animation of Jump

--- 2D_demo_1st/Assets/Scripts/Player/PlayerAnimation.cs
+++ 2D_demo_1st/Assets/Scripts/Player/PlayerAnimation.cs
@@ -6,11 +6,13 @@ public class PlayerAnimation : MonoBehaviour
 {
     private Animator anim;
     private Rigidbody2D rb;
+    private PhysicsCheck pc;
 
     private void Awake()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        pc = GetComponent<PhysicsCheck>();
     }
 
     private void Update()
@@ -21,5 +23,7 @@ public class PlayerAnimation : MonoBehaviour
     private void SetAnimation()
     {
         anim.SetFloat("velocity",Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("velocity_y", rb.velocity.y);
+        anim.SetBool("isGround", pc.isGround);
     }
 }
