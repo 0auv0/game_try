03046ea69fb53a11de9defcf685bc7f34bce16af
auv <591561890@qq.com>
2023-08-19 17:36:41 +0800

    Restart for boar

---
+++ 2D_demo_1st/Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class Enemy : MonoBehaviour
+{
+
+    protected Rigidbody2D rb;
+    protected Animator anim;
+
+    [Header("基本参数")]
+
+    public float normalSpeed;
+    public float chaseSpeed;
+    public float currentSpeed;
+
+    public Vector3 facedir;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        currentSpeed = normalSpeed;
+    }
+
+    private void Update()
+    {
+        facedir = new Vector3(-transform.localScale.x, 0, 0);
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    public virtual void Move()
+    {
+        rb.velocity = new Vector2(currentSpeed * facedir.x * Time.deltaTime,rb.velocity.y);
+    }
+}
